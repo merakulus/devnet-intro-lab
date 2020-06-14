775449cf3edd7cd79f360cdae802ce68a1d1d42b
merakulus <merakulus@gmail.com>
2020-06-15 00:18:23 +1000

    Adding HoE v0.2

---
+++ hands_on_exercise.py
@@ -0,0 +1,42 @@
+"""Intro to Python - Part 1 - Hands-On Exercise."""
+
+
+import math
+import random
+
+
+# TODO: Write a print statement that displays both the type and value of `pi`
+pi = math.pi
+print(pi)
+
+
+# TODO: Write a conditional to print out if `i` is less than or greater than 50
+i = random.randint(0, 100)
+print(i)
+if i < 50:
+    print("Lower than 50")
+else:
+    print("highet than 50")
+
+
+# TODO: Write a conditional that prints the color of the picked fruit
+picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
+print(f"Picked Fruit: {picked_fruit}")
+if picked_fruit == 'orange':
+    print("orange")
+elif picked_fruit == 'strawberry':
+    print("red")
+else:
+    print("yellow")
+
+# TODO: Write a function that multiplies two numbers and returns the result
+# Define the function here.
+
+
+# TODO: Now call the function a few times to calculate the following answers
+
+print("12 x 96 =",)
+
+print("48 x 17 =",)
+
+print("196523 x 87323 =",)
